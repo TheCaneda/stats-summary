712af703685335b4beca0d734ffa92ad00a3677d
Vitor Lemos <vitor.lemos@postmetria.com.br>
2024-02-07 18:47:39 -0300

    fix: transforms repo in python package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stats_summary',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',  # Add your dependencies here
+        'scipy',
+        'math'
+    ],
+    author='',
+    author_email='',
+    description='',
+    license='MIT',
+    keywords='stats summary',
+    url='',  
+)
