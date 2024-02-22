b2df762c95d8b8e1f91ed8d994b5355b4ed9a401
Vitor Lemos <vitor.lemos@postmetria.com.br>
2024-02-22 20:08:14 -0300

    refactor: changes showing logic for text

--- stats_summary/catalogue.py
+++ stats_summary/catalogue.py
@@ -19,42 +19,42 @@ class Catalogue:
         return test_index.keys()
 
     def show_usecases():
-        print(comparison_table)
+        return comparison_table
 
     def get_test(test_name):
-        print(test_index[test_name])
+        return test_index[test_name]
 
     def get_test_examples(test_name):
-        print(test_index[test_name].get('examples'))
+        return test_index[test_name].get('examples')
 
     def get_test_description(test_name):
-        print(test_index[test_name].get('description'))
+        return test_index[test_name].get('description')
 
     def get_test_formulas(test_name):
-        print(test_index[test_name].get('formulas'))
+        return test_index[test_name].get('formulas')
 
     def get_test_use_cases(test_name):
-        print(test_index[test_name].get('use_cases'))
+        return test_index[test_name].get('use_cases')
 
     def get_test_summary(test_name):
-        print(test_index[test_name].get('summary'))
+        return test_index[test_name].get('summary')
 
     def get_code_snippets(test_name):
-        print(test_index[test_name].get('code_snippets'))
+        return test_index[test_name].get('code_snippets')
 
     def get_thorough_examples(test_name):
-        print(test_index[test_name].get('thorough_examples'))
+        return test_index[test_name].get('thorough_examples')
 
 class BasicInference:
 
     def get_summary():
-        print(inference_summary)
+        return inference_summary
 
     def get_tailed_tests_difference():
-        print(tailed_tests_differences)
+        return tailed_tests_differences
 
     def get_tailed_tests_separate_examples():
-        print(tailed_tests_separate_examples)
+        return tailed_tests_separate_examples
 
     def get_tailed_tests_single_example():
-        print(tailed_tests_single_example)
+        return tailed_tests_single_example
